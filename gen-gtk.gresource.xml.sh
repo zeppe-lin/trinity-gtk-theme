1db0081d6e3b0fadd027308c6cc8dc8d2980c16d
Alexandr Savca <alexandr.savca89@gmail.com>
2022-12-06 05:51:33 +0200

    add gen-gtk.gresource.xml.sh

---
+++ gen-gtk.gresource.xml.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ 
+cat <<EOF
+<?xml version='1.0' encoding='UTF-8'?>
+  <gresources>
+    <gresource prefix='/org/trinityproject/gtk'>
+EOF
+ 
+find gtk-3.0 -type f -name "*.png" \
+  -printf "      <file preprocess='to-pixdata'>%P</file>\n"
+ 
+cat <<EOF
+      <file>gtk.css</file>
+    </gresource>
+  </gresources>
+EOF
+ 
+# vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
+# End of file.
